baee3d6a0cc6a19943bc932ff3ee6b54dc20ca8f
Logon Aluno <logonrmlocal@fiap.com.br>
2023-05-10 20:11:04 -0300

    Parte 3 - Ckp Flutter - 10/05

--- GABRIEL_ONISHI_RM87182/lib/screens/repo_detail_screen.dart
+++ GABRIEL_ONISHI_RM87182/lib/screens/repo_detail_screen.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:githubrepo/components/all.dart';
 import 'package:githubrepo/mocks/repositories_mock.dart';
 
+import '../constants/spacings.dart';
+import '../constants/typography.dart';
 import '../models/repository.dart';
 
 class RepoDetailScreen extends StatefulWidget {
@@ -20,6 +22,65 @@ class _RepoDetailScreenState extends State<RepoDetailScreen> {
       appBar: CustomAppBar(
         text: "Detalhes",
         hasBackButton: true,
+      ),
+      body: Container(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            Column(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                SizedBox(
+                  height: 180,
+                  width: 180,
+                  child: CustomAvatar(avatarUrl: repository.owner?.avatar),
+                ),
+                Column(
+                  children: [
+                    const CustomText(
+                      text: "Repositório",
+                      style: TypographyType.label,
+                    ),
+                    CustomText(
+                      text: "${repository.name}",
+                      style: TypographyType.header,
+                    ),
+                  ],
+                ),
+                Column(
+                  children: [
+                    const CustomText(
+                      text: "Usuário",
+                      style: TypographyType.label,
+                    ),
+                    CustomText(
+                      text: "${repository.owner?.login}",
+                      style: TypographyType.title,
+                    ),
+                  ],
+                ),
+                Column(
+                  children: [
+                    const CustomText(
+                      text: "Descrição",
+                      style: TypographyType.label,
+                    ),
+                    SizedBox(
+                      width: 350,
+                      child: CustomText(
+                        text: "${repository.description}",
+                        style: TypographyType.body,
+                        textOverflow: TextOverflow.ellipsis,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
